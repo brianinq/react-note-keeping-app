ad0a3a44f5a64ba4fc0c8afed0c33bfd4422b3ef
brianinq <73099800+brianinq@users.noreply.github.com>
2021-04-08 05:15:00 -0700

    add-components-to-app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
-import notes from "../notes";
+import Compose from "./compose";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((previousNotes) => {
+      return [...previousNotes, note];
+    });
+  }
+  function deleteNote(id) {
+    setNotes((previousNotes) => {
+      return previousNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
+      <Compose onAdd={addNote} />
+      {notes.map((noteItem, index) => (
         <Note
-          key={noteItem.key}
+          key={index}
+          id={index}
           title={noteItem.title}
           content={noteItem.content}
+          onDelete={deleteNote}
         />
       ))}
       <Footer />
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -5,6 +5,14 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <button
+        onClick={() => {
+          props.onDelete(props.id);
+          console.log(123);
+        }}
+      >
+        DELETE
+      </button>
     </div>
   );
 }
